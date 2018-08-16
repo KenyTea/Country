3da071c872b014179e6d523c0b9d556e0065a7f6
Васильева <@223_2>
2018-08-16 21:23:49 +0600

    111

--- DOC/Program.cs
+++ DOC/Program.cs
@@ -16,6 +16,7 @@ namespace DOC
         {
             HostFactory.Run(configure =>
             {
+
                 configure.Service<DocService>(service =>
                 {
                     service.ConstructUsing(s => new DocService());
